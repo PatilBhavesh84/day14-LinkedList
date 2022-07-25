a3760e821a937fc0a65d0885de09d43ab2456479
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-25 22:45:10 +0530

    Added:Inserted 30 between 70 & 56

--- src/com/BridgeLabz/LinkedList.java
+++ src/com/BridgeLabz/LinkedList.java
@@ -13,6 +13,11 @@ public class LinkedList {
 		public Node(int value) {
 			this.value = value;
 		}
+
+		public Node(int value, Node next) { 
+			this.value = value;
+			this.next = next;
+		}
 	}
 
 	public LinkedList() {
@@ -20,13 +25,14 @@ public class LinkedList {
 	}
 
 	public void display() {
-		
 		Node temp = head;
 		while (temp != null) {
-			System.out.print(temp.value+"->");
+			System.out.print(temp.value +"-");
 			temp = temp.next;
 		}
+		System.out.println();
 	}
+
 	public void addFirst(int val) {
 		Node node = new Node(val);
 		node.next = head;
@@ -37,6 +43,7 @@ public class LinkedList {
 		}
 		size += 1;
 	}
+
 	public void addLast(int val) {
 		if (tail == null) {
 			addFirst(val);
@@ -45,7 +52,27 @@ public class LinkedList {
 		Node node = new Node(val);
 		tail.next = node;
 		tail = node;
-
-
+		size++;
 	}
+
+	public void insertValAtIndex(int val, int index) {
+		if (index == 0) {
+			addFirst(val);
+			return;
+		}
+		if (index == size) {
+			addLast(val);
+			return;
+		}
+
+		Node temp = head;
+		for (int i = 1; i < index; i++) {
+			temp = temp.next;
+		}
+
+		Node node = new Node(val, temp.next);
+		temp.next = node;
+		size++;
+	}
+
 }
--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -1,12 +1,13 @@
 package com.BridgeLabz;
 
 public class Main {
-	
-		public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.addFirst(56);
-		list.addLast(30);
 		list.addLast(70);
+		list.display();
+		list.insertValAtIndex(30,1);
 		list.display();
 	}
 }
