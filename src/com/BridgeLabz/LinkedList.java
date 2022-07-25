c0d7838ef5dc5874914d3a0be2bdba8033ff776a
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-25 22:04:24 +0530

    Added:Created LinkedList

--- src/com/BridgeLabz/LinkedList.java
+++ src/com/BridgeLabz/LinkedList.java
@@ -2,7 +2,38 @@ package com.BridgeLabz;
 
 public class LinkedList {
 
-	public static void main(String[]args) {
-		System.out.println("linkedList");
+	Node head;
+	Node tail;
+	int size;
+
+	class Node {
+		int value;
+		Node next;
+
+		public Node(int value) {
+			this.value = value;
+		}
+	}
+
+	public LinkedList() {
+		this.size = 0;
+	}
+
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
+	public void addValue(int val) {
+		Node node = new Node(val);
+		node.next = head;
+		head = node;
+
+		if (tail == null) {
+			tail = head;
+		}
+		size += 1;
 	}
 }
---
+++ src/com/BridgeLabz/Main.java
@@ -0,0 +1,12 @@
+package com.BridgeLabz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.addValue(70);
+		list.addValue(30);
+		list.addValue(56);
+		list.display();
+	}
+}
